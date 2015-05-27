0448740128c6763ec1b0c7db23e9408d2c44cf2d
Guizrio <guigzrio@gmail.com>
2015-05-27 02:11:55 +0200

    Initialyze Project

---
+++ Swapper.java
@@ -0,0 +1,193 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carsequencing;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author g.rio
+ */
+public class Swapper extends Algo{
+    
+    private ArrayList<Long> objViolAtPosition;   //save alls objective violation for all positions 
+                                               //    (in order to don't recalculate violations along entirely vector of cars.
+    private long multOfPaintViolations;     //keep the number multiplier of paint violation (objective function)
+    
+    long iViolationsAfter;     //Used to keep in memory violations costs after swap() call (because objViolAtPosition isn't be refreshed automatically --> more efficient)
+    long jViolationsAfter;
+    
+    public Swapper(DataProblem dat) {
+        super(dat);
+        multOfPaintViolations = dat.getClassObjective().getMultForCompute()[2];
+        objViolAtPosition = new ArrayList<>();
+        for (int i = 0; i < dat.getNbCars(); i++) {
+            objViolAtPosition.add(objViolAtPos(i, dat.getCars()));
+        }
+    }
+    
+    
+    /**
+     * Perform a swap between car i and car j and return Solution.
+     * <p>
+     * @param sol THe solution where cars must be swapped
+     * @param i car to be exchanged with car j
+     * @param j car to be exchanged with car i
+     * @return the Solution os swap.
+     */
+    public Solution swap(Solution sol, int i, int j){
+        Time timeStart = new Time();
+        ArrayList<Car> cars = new ArrayList<Car>(sol.getCars());
+        dat.getClassObjective().getMultForCompute();
+
+        long iViolationsBefore = objViolAtPosition.get(i);
+        long jViolationsBefore = objViolAtPosition.get(j);
+                
+        Car car1 = new Car(cars.get(i));
+        
+        cars.set(i, cars.get(j));
+        cars.set(j, car1);
+        
+        iViolationsAfter = objViolAtPos(i, cars);
+        jViolationsAfter = objViolAtPos(j, cars);
+        
+        //objViolAtPosition.set(i, iViolationsAfter);    //We don't set now because it's not necessary if result of swap does'nt give better solution...
+        //objViolAtPosition.set(j, jViolationsAfter);       //NOte : if we plan to call more than one swap at a time without check and modify objViolAtPosition, it's more complicated,
+                                                            //      and maybe in this case it's better to automatically set objViolAtPosition here (and eventually to deset it after...
+        
+        //We have to compute the new paint violation number
+        long nbTotalViol=0;
+        int color = cars.get(0).getPaintColor();
+        int sameColor=0;
+        for (int k = 0; k < dat.getNbCars(); k++) {
+            if(color == cars.get(k).getPaintColor()){
+                if(sameColor == dat.getMaxSamePainting()+1){
+                    sameColor=1;
+                    nbTotalViol++;
+                }
+            }else{
+                nbTotalViol++;
+                color=cars.get(k).getPaintColor();
+                sameColor=1;
+            }
+        }
+        
+        long objValue = sol.getObjSol() - iViolationsBefore - jViolationsBefore
+                                        + iViolationsAfter + jViolationsAfter
+                                        + nbTotalViol * multOfPaintViolations;
+        
+        Solution solBis = new Solution(cars, objValue, sol.getPartialObjSol()[0],
+                sol.getPartialObjSol()[1], sol.getPartialObjSol()[2], 
+                new Time().timeLongElapsedSince(timeStart.getLastSavedTime()) 
+                        + sol.getTimeToSolve());
+        
+        
+        
+        return solBis;
+    }
+    
+    /**
+     * Give the objective violation for high and low priority at position i
+     * of an sequenced car list.
+     * <p>
+     * Note that like paint batches does'nt have a "window" in which count the number
+     * of violations (except window which represent all cars), the result of this
+     * function don't contain any paint batches violations.
+     * <p>
+     * @param i the index of the sequenced car in list where we want the objective violation.
+     * @return Objective violation at position i
+     */
+    private long objViolAtPos(int i, ArrayList<Car> shedulCars){
+        long nbTotalViol[] = new long[2]; //Order : highprio, lowprio, paint batches
+        int[] multObjective = dat.getClassObjective().getMultForCompute();        
+        
+        ArrayList<RatioConstraint> highPrioConst = dat.getHighConst();
+        ArrayList<RatioConstraint> lowPrioConst = dat.getLowConst();        
+        
+        //Count number of HighPriority constraints violations
+        nbTotalViol[0] = 0;
+        for (RatioConstraint ratConst : highPrioConst) {
+            int startPossibleViol = Math.max(i - ratConst.getWindowSize() + 1, 0);    //First position where we could found an objective violation in window around i
+            //int endPossibleViol = Math.min(i + ratConst.getWindowSize()-1, dat.getNbCars()-1);    //Last position where we could found an objective violation in window around i
+
+            for (int j = startPossibleViol; j <= i; j++) { //Position of start point window
+                int min = Math.min(ratConst.getWindowSize(), dat.getNbCars() - j); //Because lasts windows are "incompletes" (not same length)
+                int nbCarShedulInWindow = 0;
+                for (int k = j; k < j+min; k++) {
+                    if (shedulCars.get(k).getRatioConstraint().contains(ratConst)){
+                        nbCarShedulInWindow++;
+                    }
+                    nbTotalViol[0]+=Math.max(0,nbCarShedulInWindow - ratConst.getMaxCarInWindow());
+                }
+            }
+        }
+        
+        //Count number of LowPriority constraints violations
+        nbTotalViol[1] = 0;
+        for (RatioConstraint ratConst : lowPrioConst) {
+            int startPossibleViol = Math.max(i - ratConst.getWindowSize() + 1, 0);    //First position where we could found an objective violation in window around i
+            //int endPossibleViol = Math.min(i + ratConst.getWindowSize() - 1, dat.getNbCars());    //Last position where we could found an objective violation in window around i
+            
+            for (int j = startPossibleViol; j <= i; j++) { //Position of start point window
+                int min = Math.min(ratConst.getWindowSize(), dat.getNbCars() - j); //Because lasts windows are "incompletes" (not same length)
+                int nbCarShedulInWindow = 0;
+                for (int k = j; k < j+min; k++) {
+                    if (shedulCars.get(k).getRatioConstraint().contains(ratConst)){
+                        nbCarShedulInWindow++;
+                    }
+                    nbTotalViol[1]+=Math.max(0,nbCarShedulInWindow - ratConst.getMaxCarInWindow());
+                }
+            }
+        }
+        
+        return nbTotalViol[0] * multObjective[0]
+                + nbTotalViol[1] * multObjective[1];
+    }
+
+    @Override
+    public Solution solve() {
+        Time timeStart = new Time();
+        
+        ArrayList<Car> shedulCars = new ArrayList<>(dat.getCars());
+        Solution incumbent = InitialyzeSolutionValue(shedulCars, timeStart);
+        
+        System.out.println("\n\n");
+        System.out.println("Total number of cars = " + shedulCars.size());
+        System.out.println("Number of cars which must be sheduled : " + dat.getNbCarsDayJ());
+        System.out.println("Initial Solution Value = " + incumbent.getObjSol());
+        System.out.println("");
+        
+        long nbIterations = 0; //number of iterations performed by algorithm
+        
+        //First we takes just car which have to be sorted
+        while(new Time().timeLongElapsedSince(timeStart.getLastSavedTime()) <= 1000000000l){
+            nbIterations++;
+            
+            int randi = (int)(Math.random()*dat.getNbCarsDayJ())+dat.getNbCarsDayJMinus1();
+            int randj = (int)(Math.random()*dat.getNbCarsDayJ())+dat.getNbCarsDayJMinus1();
+            
+            //To be erased
+            if(randi>499) System.out.println("randi = " + randi);
+            if(randi<0) System.out.println("randi = " + randi);
+            if(randj>499) System.out.println("randj = " + randj);
+            if(randj<0) System.out.println("randj = " + randj);
+            
+            
+            Solution solToTest = swap(incumbent, randi, randj);
+            
+            if(incumbent.getObjSol() > solToTest.getObjSol()){
+                incumbent = solToTest;
+                objViolAtPosition.set(randi, iViolationsAfter);    
+                objViolAtPosition.set(randj, jViolationsAfter);
+            }
+            
+        }
+        System.out.println("Nombre d'itérations : " + nbIterations);
+        return incumbent;
+    }
+}
---
+++ Time.java
@@ -0,0 +1,372 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carsequencing;
+
+import static java.lang.Thread.sleep;
+
+/**
+ *Class use to save time and return with different format, or to time applications.
+ * <p>
+ * les temps entrés doivent être en nanosecondes.<p>
+ *ATTENTION car les temps sont arrondi à la milliseconde (convention 0,5 arrondi à 1)
+ * Cela signifie qu'un temps passé en paramètres en nanosecondes égal à 10,5 millisecondes sera
+ * redonné par la méthode getNbMilliSec() comme égal à 11 millisecondes.
+ * <p>
+ * Note : le nombre d'heures partielles est égal au nombre d'heures totales...
+ * <p>
+ * Si on souhaite reconstruire un temps customizé, les fonctions getPartials*() sont utiles.
+ * <p>
+ * Pour avoir des troncatures à une unité choisie, les fonctions getNb*() sont utiles.
+ * <p>
+ * Les méthodes timeElapsed*() modifient le time de la classe et servent de chronomètre,
+ * ou pour calculer un espacement de temps entre deux temps.
+ * 
+ * @author Guillaume
+ */
+public class Time {
+    
+    private long time;
+    private long lastSaveTime;
+    
+    //variables dont la somme constitue le temps total
+    private long hours;
+    private int minutes;
+    private int secondes;
+    private int millisecondes;
+    
+    //Variables de temps total
+    private long totalMinutes;
+    private long totalSecondes;
+    private long totalMilliSec;
+    
+    
+    /**
+     * Contruct a new Time from the current JVM time.
+     */
+    public Time(){
+        this.time = System.nanoTime();
+        
+        this.hours = 0l;
+        this.minutes = 0;
+        this.secondes = 0;
+        this.millisecondes = 0;
+        
+        this.totalMinutes = 0l;
+        this.totalSecondes = 0l;
+        this.totalMilliSec = 0l;
+        convertTime();
+    }
+    
+    /**
+     * Construct the new Time from a time in nanosecond.
+     * @param time a nanosecond time to be converted.
+     */
+    public Time(long time){
+        this.time = time;
+        
+        this.hours = 0l;
+        this.minutes = 0;
+        this.secondes = 0;
+        this.millisecondes = 0;
+        
+        this.totalMinutes = 0l;
+        this.totalSecondes = 0l;
+        this.totalMilliSec = 0l;
+        convertTime();
+    }
+    
+    /**
+     * Function to initialize all variables except time.
+     */
+    private void convertTime(){
+        long reste = time / 100000l;
+        
+        hours = reste / 36000000l;
+        reste -= hours * 36000000l;
+        minutes = (int) (reste / 600000l);
+        
+        totalMinutes = hours*60l + minutes;
+        
+        reste -= (long)minutes * 600000l;
+        secondes = (int) (reste / 10000l);
+        
+        totalSecondes = totalMinutes * 60l + secondes;
+        
+        reste -= (long)secondes * 10000l;
+        millisecondes = (int)Math.round((double)reste / (double)10);
+        
+        totalMilliSec = totalSecondes * 1000l + (long)millisecondes;
+        lastSaveTime = time;
+    }
+    
+    /**
+     * Display the time elapsed since value initialization in constructor
+     * or last timeElaps*() call.
+     * <p>
+     * If no timeElaps*() call was done between initialisation and now, then :
+     * <MENU>
+     * if this instance was created with empty construtor, it display time
+     * elapsed between the creation of this instance and now.
+     * <p>
+     * if this instance was created with constructor Time(long #time), it display
+     * time elapsed between #time and now.
+     * </MENU>
+     * Otherwise, it display time elapsed since last call of timeElaps*() method.
+     * <p>
+     * after the call the current time of this instance is set to the time of end of call,
+     * but there aren't recalculation of all other variables, so it's possible after
+     * a timeElaps*() call (let be T1) to use methods to get hours, minutes,... and they would give
+     * the time between "creation of instance or #time or last timeElaps*() call" and this timeElaps*()
+     * call (T1), in their respective unit.
+     * 
+     * @return time between (creation of instance or #time or last timeElaps*() call) and now.
+     */
+    public String timeElapsed(){
+        this.time = System.nanoTime() - this.time;
+        convertTime();
+        this.time = System.nanoTime();
+        return toString();
+    }
+    
+    /**
+     * Display the time elapsed since timeStart.
+     * <p>
+     * after the call the current time of this instance is set to the time of end of call.
+     * but there aren't recalculation of all other variables, so it's possible after
+     * a timeElaps*() call (let be T1) to use methods to get hours, minutes,... and they would give
+     * the time between "timeStart" and this timeElaps*() call (T1), in their respective unit.
+     * 
+     * @param timeStart the precedent time in nanosecond.
+     * @return time elapsed between timeStart and now.
+     */
+    public String timeElapsedSince(long timeStart){
+        this.time = System.nanoTime() - timeStart;
+        convertTime();
+        this.time = System.nanoTime();
+        return toString();
+    }
+    
+    public long timeLongElapsedSince(long timeStart){
+        this.time = System.nanoTime() - timeStart;
+        convertTime();
+        this.time = System.nanoTime();
+        return lastSaveTime;
+    }
+    
+    /**
+     * Display the time elapsed between two times.
+     * <p>
+     * after the call the current time of this instance is set to the time of end of call.
+     * but there aren't recalculation of all other variables, so it's possible after
+     * a timeElapsedBetween("timeStart", "timeEnd") call (let be T1) to use methods to get hours, minutes,...
+     * and they would give the time between "timeStart" and "timeEnd", in their respective unit.
+     * 
+     * @param timeStart the start time in nanoseconds
+     * @param timeEnd the end time in nanoseconds
+     * @return time elapsed between timeStart and timeEnd.
+     */
+    public String timeElapsedBetween(long timeStart, long timeEnd){
+        this.time = timeEnd - timeStart;
+        convertTime();
+        this.time = System.nanoTime();
+        return toString();
+    }
+    
+    /**
+     * Return last saved time in nanoseconds.
+     * <p>
+     * It correspond to time given by methods getNb*() and getPartial*()
+     * @return last saved time in nanoseconds.
+     */
+    public long getLastSavedTime(){
+        return lastSaveTime;
+    }
+    
+    /**
+     * Get the total time in hours.
+     * <p>
+     * Note : all time under hour is not displayed (1h59m give 1 hour)
+     * <p>
+     * This method doesn't have a dual PartialNbhours() method because
+     * hour is the highest range in time use here. So they are a same method.
+     * 
+     * @return total time in hours.
+     */
+    public long getNbHours(){
+        return hours;
+    }
+    
+    /**
+     * Get the total time in minutes.
+     * <p>
+     * Note : all time under minute is not displayed (1m59s give 1 minute)
+     * <p>
+     * * To reconstruct time from this instance, use Partials method instead.
+     * 
+     * @return the total time in minutes.
+     */
+    public long getNbMinutes(){
+        return totalMinutes;
+    }
+    
+    /**
+     * Get the total time in seconds.
+     * <p>
+     * Note : all time under second is not displayed (1s999ms give 1 second)
+     * <p>
+     * To reconstruct time from this instance, use Partials method instead.
+     * 
+     * @return the total time in seconds. 
+     */
+    public long getNbSecondes(){
+        return totalSecondes;
+    }
+    
+    /**
+     * Get the total time in milliseconds.
+     * @return the total time in milliseconds.
+     */
+    public long getNbMilliSec(){
+        return totalMilliSec;
+    }
+    
+    
+    /**
+     * Get partial time correspond to m in the time
+     * h:m:s:ms.
+     * 
+     * @return partial time correspond to m in the time h:m:s:ms.
+     */
+    public int getPartialNbMinutes(){
+        return minutes;
+    }
+    
+    
+    /**
+     * Get partial time correspond to s in the time
+     * h:m:s:ms.
+     * 
+     * @return partial time correspond to s in the time h:m:s:ms.
+     */
+    public int getPartialNbSeconds(){
+        return secondes;
+    }
+    
+    
+    /**
+     * Get partial time correspond to ms in the time
+     * h:m:s:ms.
+     * 
+     * @return partial time correspond to ms in the time h:m:s:ms.
+     */
+    public int getPartialMilliSec(){
+        return millisecondes;
+    }
+    
+    @Override
+    public String toString(){
+        
+        if(hours != 0l){
+            
+            return hours + "h " + minutes + "m " + secondes + "s " + millisecondes +"ms";
+            
+        }else if(minutes != 0l){
+            
+            return minutes + "m " + secondes + "s " + millisecondes +"ms";
+            
+        }else if(secondes != 0l){
+            
+            return secondes + "s " + millisecondes +"ms";
+            
+        }else{
+            
+            return millisecondes +"ms";
+        }
+        //return hours + "h " + minutes + "m " + secondes + "s " + millisecondes +"ms";
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+        
+        long time = 120*1000000000l*3600l + 59*1000000000l*60l + 4*1000000000l + 3*100000000l + 8*10000000l + 9*1000000l + 1*499999l; //le dernier est en dessous de la millisecondes
+        Time timeconvert = new Time(time);
+        
+        System.out.println(timeconvert);
+        System.out.println(new Time(timeconvert.getLastSavedTime()));
+        System.out.println("Total time (tronqued): ");
+        System.out.println("\thours : " + timeconvert.getNbHours());
+        System.out.println("\tminutes : " + timeconvert.getNbMinutes());
+        System.out.println("\tseconds : " + timeconvert.getNbSecondes());
+        System.out.println("\tmilliseconds " + timeconvert.getNbMilliSec());
+        System.out.println("Partials times : ");
+        System.out.println("\thours : " + timeconvert.getNbHours());
+        System.out.println("\tminutes : " + timeconvert.getPartialNbMinutes());
+        System.out.println("\tseconds : " + timeconvert.getPartialNbSeconds());
+        System.out.println("\tmilliseconds " + timeconvert.getPartialMilliSec());
+        System.out.println("===============================================");
+        System.out.println("Chrono :");
+        timeconvert.timeElapsed(); // ou timeconvert = new Time();
+        
+        System.out.println(timeconvert.timeElapsed());
+        System.out.println(new Time(timeconvert.getLastSavedTime()));
+        System.out.println("Total time (tronqued): ");
+        System.out.println("\thours : " + timeconvert.getNbHours());
+        System.out.println("\tminutes : " + timeconvert.getNbMinutes());
+        System.out.println("\tseconds : " + timeconvert.getNbSecondes());
+        System.out.println("\tmilliseconds " + timeconvert.getNbMilliSec());
+        System.out.println("Partials times : ");
+        System.out.println("\thours : " + timeconvert.getNbHours());
+        System.out.println("\tminutes : " + timeconvert.getPartialNbMinutes());
+        System.out.println("\tseconds : " + timeconvert.getPartialNbSeconds());
+        System.out.println("\tmilliseconds " + timeconvert.getPartialMilliSec());
+        System.out.println("===============================================");
+        
+        sleep(1051); //methode sleep peu précise mais c'est pour l'idée ! =)
+        
+        System.out.println("");
+        System.out.println(timeconvert.timeElapsed());
+        System.out.println(new Time(timeconvert.getLastSavedTime()));
+        System.out.println("Total time (tronqued): ");
+        System.out.println("\thours : " + timeconvert.getNbHours());
+        System.out.println("\tminutes : " + timeconvert.getNbMinutes());
+        System.out.println("\tseconds : " + timeconvert.getNbSecondes());
+        System.out.println("\tmilliseconds " + timeconvert.getNbMilliSec());
+        System.out.println("Partials times : ");
+        System.out.println("\thours : " + timeconvert.getNbHours());
+        System.out.println("\tminutes : " + timeconvert.getPartialNbMinutes());
+        System.out.println("\tseconds : " + timeconvert.getPartialNbSeconds());
+        System.out.println("\tmilliseconds " + timeconvert.getPartialMilliSec());
+        System.out.println("===============================================");
+        
+        timeconvert = new Time();
+        
+        sleep(10);    //methode sleep peu précise mais c'est pour l'idée ! =)
+        System.out.println("Time elapsed : ");
+        System.out.println(timeconvert.timeElapsed());
+        System.out.println(new Time(timeconvert.getLastSavedTime()));
+        
+        sleep(10);  //methode sleep peu précise mais c'est pour l'idée ! =)
+        System.out.println("Time elapsed : ");
+        System.out.println(timeconvert.timeElapsed());
+        System.out.println(new Time(timeconvert.getLastSavedTime()));
+        
+        sleep(10);  //methode sleep peu précise mais c'est pour l'idée ! =)
+        System.out.println("Time elapsed : ");
+        System.out.println(timeconvert.timeElapsed());
+        System.out.println(new Time(timeconvert.getLastSavedTime()));
+        System.out.println("");
+        
+        long time1 = 3*1000000000l*3600l + 25*1000000000l*60l;
+        time = 3*1000000000l*3600l + 26*1000000000l*60l + 15*1000000000l;
+        System.out.println("time1 = " + new Time(time1));
+        System.out.println("time = " + new Time(time));
+        System.out.println("Time elapsed between time1 and time: ");
+        System.out.println(timeconvert.timeElapsedBetween(time1, time));
+        System.out.println(new Time(timeconvert.getLastSavedTime()));
+        
+        
+    }
+    
+}
